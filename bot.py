d20e90ae76afd525018ac8a96233dd0eb1aedfa7
Colin Wilson <github@colinwilson.dev>
2022-01-13 19:11:29 -0500

    DEV: Auto code-block works (#2)

--- bot.py
+++ bot.py
@@ -55,6 +55,8 @@ async def on_message(message):
 
     if message.author.id in config["blacklist"]:
         return
+    
+    await autoCodeBlock.check_message(message)
 
     await bot.process_commands(message)
 
--- noncommands/auto_code_block.py
+++ noncommands/auto_code_block.py
@@ -5,23 +5,28 @@ class AutoCodeBlock:
     def __init__(self, bot):
         self.bot = bot
 
-    async def check_message(self, context):
-        message_text = context.message.content
-        if re.search('({[^]*})', message_text) and ("```" not in message_text or "```\n" in message_text):
+    async def check_message(self, message):
+        message_text = message.content
+        if re.search('\{*.\}', message_text) and ("```" not in message_text or "```\n" in message_text):
             probable_code = message_text.replace("```", "")
-            reply = "It seems like that message might contain some unformatted code, I did my best to format it for you. If this is unwanted, react to this message."
+            reply = "It seems like that message might contain some unformatted code, I did my best to format it for you. If this is unwanted, react to this message within 2 minutes."
             reply += "\n```java\n"
             reply += probable_code
             reply += "\n```\n"
             reply += "(If you are curious how to do this, check out https://www.codegrepper.com/code-examples/whatever/discord+syntax+highlighting)"
-            reply += f'Original Message sent by: {context.message.author.username}'
+            reply += f'\nOriginal Message sent by: {message.author.mention}'
 
-            msg = await context.reply(reply)
+            msg = await message.reply(reply)
+            await msg.add_reaction("🚫")
+
+            def check(reaction, user):
+                return user == message.author and str(reaction) in "🚫"
 
             try:
-                await self.bot.wait_for("reaction_add", timeout=120)
-                msg.delete()
+                await self.bot.wait_for("reaction_add", timeout=120, check=check)
+                await msg.delete()
             except asyncio.exceptions.TimeoutError:
+                await msg.clear_reactions()
                 pass
 
 
