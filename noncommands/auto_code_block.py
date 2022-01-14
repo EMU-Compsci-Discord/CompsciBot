6158d7b07fe86ca125426371d206714617acb1df
Colin Wilson <github@colinwilson.dev>
2022-01-13 20:17:34 -0500

    Refactor auto code-block

--- noncommands/auto_code_block.py
+++ noncommands/auto_code_block.py
@@ -7,8 +7,9 @@ class AutoCodeBlock:
 
     async def check_message(self, message):
         message_text = message.content
-        if re.search('\{*.\}', message_text) and ("```" not in message_text or "```\n" in message_text):
+        if self.looks_like_unformatted_code(message_text):
             probable_code = message_text.replace("```", "")
+            
             reply = "It seems like that message might contain some unformatted code, I did my best to format it for you. If this is unwanted, react to this message within 2 minutes."
             reply += "\n```java\n"
             reply += probable_code
@@ -28,6 +29,9 @@ class AutoCodeBlock:
             except asyncio.exceptions.TimeoutError:
                 await msg.clear_reactions()
                 pass
+    
+    def looks_like_unformatted_code(self, text):
+        return re.search('\{*.\}', text) and ("```" not in text or "```\n" in text)
 
 
 
