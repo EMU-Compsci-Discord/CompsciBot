cace804d4b76f75a1882d5cc494e79b5a103e497
Chiara Oxender <46496608+coxender@users.noreply.github.com>
2022-03-31 22:54:15 -0400

    impl: Security features
    - set channelparse command to only be called by admin
    - basic sanitize inputs with hardcoded directory and only alpha-numeric filenames
    -adds .csv file extension manually

--- channel.py
+++ channel.py
@@ -4,6 +4,7 @@ import csv
 import yaml
 from discord.ext import commands
 from discord.ext.commands import has_permissions
+from discord.utils import find
 import re
 
 if "CompsciBot" not in str(os.getcwd()):
@@ -15,16 +16,60 @@ with open("config.yaml") as file:
 testGuild = 931309621027684413
 
 
+# rename to something more accurate
 class ChannelManager(commands.Cog, name="channelmanager"):
     def __init__(self, bot):
         self.bot = bot
 
     async def createCategory(category, context):
+        """
+        creates a category
+
+            Parameters:
+                category: the name of the category
+                context: the context of the command
+
+            Returns:
+                the category object
+        """
+
         guild = context.guild
+
         try:
             return await guild.create_category(category)
         except:
             print("Issue with ", category, ".  Error: ", sys.exc_info()[0])
+
+    async def getCategory(category_name, context):
+        """
+        Finds a category by same or if none exists creates a new one
+            Parameters:
+                category: the name of the category
+                context: the context of the command
+
+            Returns:
+                the category object
+        """
+        guild = context.guild
+        category = find(lambda category: category.name ==
+                        category_name, guild.categories)
+        # get category by category name
+        if(category == None):
+            category = await guild.create_category(category_name)
+        return category
+
+    async def createChannel(channel_name: str, category_name: str, context):
+        """
+        Given a channel and a category name, creates a channel and assigns it to the category
+            Parameters:
+                channel_name: the name of the channel
+                category_name: the name of the category
+        """
+        guild = context.guild
+
+        category = await ChannelManager.getCategory(category_name, context)
+
+        return await guild.create_text_channel(channel_name, category=category)
 
     def makeDict(categories, channels):
         classDict = {}
@@ -44,6 +89,8 @@ class ChannelManager(commands.Cog, name="channelmanager"):
 
         channelnames = []
         categories = []
+
+        # parses command to get message content
         prefix = config["bot_prefix"]
         startLen = len(prefix) + len("channelparse")
         filename = context.message.content[startLen:].strip()
@@ -52,12 +99,18 @@ class ChannelManager(commands.Cog, name="channelmanager"):
             await context.send("Please input a filename without special characters or extensions")
             return
 
+        # default csv
+        if (not filename.strip()):
+            filename = 'testcsv'
         filename = "resources\\"+filename+".csv"
         print("filename: ", filename)
 
+        # read and parse the csv
         with open(filename, newline='') as csvfile:
             csvreader = csv.reader(csvfile, delimiter=',')
             for row in csvreader:
+
+                # check if those attributes exist
                 if(row[2].strip() and row[3].strip() and row[19].strip()):
 
                     classtype = row[2]
@@ -68,6 +121,7 @@ class ChannelManager(commands.Cog, name="channelmanager"):
                     if (prof != "TBA"):
                         prof = prof.split()[1]
 
+                    # assemble class and category names
                     channelname = classtype+"-"+classnum+"-"+prof
                     categoryname = classtype + "-"+classnum
 
@@ -81,6 +135,18 @@ class ChannelManager(commands.Cog, name="channelmanager"):
                 if('388' in channel or '571' in channel or '511' in channel):
                     continue
                 await ChannelManager.createChannel(channel, classDict[channel], context)
+
+        await context.send("Channels Created Successfully")
+
+    @commands.command(name="deleteAll")
+    async def deleteAll(self, context):
+        """
+        Deletes all channels in Test Discord that aren't in TextChanne's and VoiceChannels category
+        """
+        if context.guild.id == testGuild:  # WILL NOT RUN IN COMPSCI
+            for channel in context.guild.channels:
+                if channel.category.id != 931309621770080388 or channel.category.id != 931309621770080389:
+                    channel.delete()
 
 
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
