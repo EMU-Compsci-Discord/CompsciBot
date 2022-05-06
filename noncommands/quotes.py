d116575f83ae999011ec61c60cde28992ec1f3ba
Chiara Oxender <46496608+coxender@users.noreply.github.com>
2022-05-05 22:22:18 -0400

    feat: case insensitive searches

--- noncommands/quotes.py
+++ noncommands/quotes.py
@@ -2,21 +2,24 @@ import json
 import random
 import yaml
 import os
+import re
 
 if "CompsciBot" not in str(os.getcwd()):
     os.chdir("./CompsciBot")
 with open("config.yaml") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
+
 class Quotes:
     def __init__(self, bot):
-        self.bot=bot
+        self.bot = bot
+
     async def dailyQuote(self):
-        c=self.bot.get_channel(707293854507991172)
-        dailyquote=await Quotes.quote(self.bot,"")
+        c = self.bot.get_channel(707293854507991172)
+        dailyquote = await Quotes.quote(self.bot, "")
         await c.send("Daily Quote:\n"+dailyquote)
 
-    async def quote(self,keywords):
+    async def quote(self, keywords):
 
         f = open("resources/quotes.json", encoding="utf-8")
         json_data = json.load(f)
@@ -24,9 +27,9 @@ class Quotes:
         matches = []
 
         if keywords:
-            random_quote="Sorry! You made a bad search"
+            random_quote = "Sorry! You made a bad search"
             for line in quotes:
-                if keywords in line:
+                if re.search(keywords, line, re.IGNORECASE):
                     matches.append(line)
             if matches:
                 random_quote = random.choice(matches)
@@ -34,20 +37,15 @@ class Quotes:
             random_quote = random.choice(quotes)
         return random_quote
 
-        
-
     async def newquote(self, context):
         prefix = config["bot_prefix"]
         startLen = len(prefix) + len("newquote")
         quote = context.message.content[startLen:]
 
-        with open("resources/quotes.json","r",encoding="utf-8") as qfile:
+        with open("resources/quotes.json", "r", encoding="utf-8") as qfile:
             qjson = json.load(qfile)
             qjson["teacherQuotes"].append(quote)
-            qfile = open("resources/quotes.json","w")
+            qfile = open("resources/quotes.json", "w")
             json.dump(qjson, qfile)
 
         return quote
-
-
-    
