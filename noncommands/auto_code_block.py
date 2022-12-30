3679616dc77a8f1c54f12fa90c6a5a82fd72beff
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-12-30 13:32:15 -0500

    feat: ✨ improve code regex
    no longer messes with messages that contain triple backtick, now can see multi-line codeblocks

--- noncommands/auto_code_block.py
+++ noncommands/auto_code_block.py
@@ -1,6 +1,8 @@
 import re
 import asyncio
 
+CODE_REGEX = re.compile(r"{.*;.*}", re.DOTALL)
+
 
 class AutoCodeBlock:
     def __init__(self, bot):
@@ -9,11 +11,9 @@ class AutoCodeBlock:
     async def check_message(self, message):
         message_text = message.content
         if self.looks_like_unformatted_code(message_text):
-            probable_code = message_text.replace("```", "")
-
             reply = "It seems like that message might contain some unformatted code, I did my best to format it for you. If this is unwanted, react to this message within 2 minutes."
             reply += "\n```java\n"
-            reply += probable_code
+            reply += message_text
             reply += "\n```\n"
             reply += "(If you are curious how to do this, check out https://www.codegrepper.com/code-examples/whatever/discord+syntax+highlighting)"
             reply += f'\nOriginal Message sent by: {message.author.mention}'
@@ -32,4 +32,4 @@ class AutoCodeBlock:
                 pass
 
     def looks_like_unformatted_code(self, text):
-        return re.search('\{*.\}', text) and ("```" not in text or "```\n" in text)
+        return CODE_REGEX.search(text) and "```" not in text
